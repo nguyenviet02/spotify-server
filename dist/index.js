a53ed02b28220054ca97704e28b1cf4ce3bb4b48
Nguyen Viet <nguyenviet02.dev@gmail.com>
2024-07-24 19:38:12 +0700

    feat: add cors

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,34 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import 'dotenv/config';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 import globalQuery from './graphql/TypeDefinitions.js';
 import globalResolvers from './graphql/GlobalResolvers.js';
+import 'dotenv/config';
 import connectToDB from './db/dbConnector.js';
 import { getUser } from './graphql/utils/index.js';
+const app = express();
 const PORT = process.env.PORT;
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
     typeDefs: globalQuery,
     resolvers: globalResolvers,
-    introspection: true,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
-const { url } = await startStandaloneServer(server, {
-    listen: { port: Number(PORT) },
+await server.start();
+await connectToDB();
+app.use('/', cors({
+    origin: '*',
+    credentials: true
+}), express.json({ limit: '50mb' }), expressMiddleware(server, {
     context: async ({ req, res }) => {
         const token = req.headers.authorization || '';
         const refreshToken = req.headers['x-refresh-token'];
         const user = await getUser(token, refreshToken, res);
         return user;
     },
-});
-await connectToDB();
-console.log(`🚀  Server ready at: ${url}`);
+}));
+await new Promise((resolve) => httpServer.listen({ port: Number(PORT) }, resolve));
+console.log(`🚀 Server ready at http://localhost:${PORT}/`);
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,51 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import 'dotenv/config';
-
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 import globalQuery from './graphql/TypeDefinitions.js';
 import globalResolvers from './graphql/GlobalResolvers.js';
+import 'dotenv/config';
 import connectToDB from './db/dbConnector.js';
 import { getUser } from './graphql/utils/index.js';
 
-// Khởi tạo server apollo
+
+interface MyContext {
+  token?: String;
+}
+
+// Required logic for integrating with Express
+const app = express();
 const PORT = process.env.PORT;
-const server = new ApolloServer({
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer<MyContext>({
   typeDefs: globalQuery,
   resolvers: globalResolvers,
-	introspection: true,
-});
-const { url } = await startStandaloneServer(server, {
-  listen: { port: Number(PORT) },
-  context: async ({ req, res }) => {
-    const token = req.headers.authorization || '';
-		const refreshToken = req.headers['x-refresh-token'] as string;
-    const user = await getUser(token, refreshToken, res);
-    return user ;
-  },
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-// Kết nối tới database
+await server.start();
 await connectToDB();
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use(
+  '/',
+  cors<cors.CorsRequest>({
+		origin: '*',
+		credentials: true
+	}),
+  express.json({ limit: '50mb' }),
+  expressMiddleware(server, {
+    context: async ({ req, res }) => {
+			const token = req.headers.authorization || '';
+			const refreshToken = req.headers['x-refresh-token'] as string;
+			const user = await getUser(token, refreshToken, res);
+			return user ;
+		},
+  }),
+);
+
+// Modified server startup
+await new Promise<void>((resolve) => httpServer.listen({ port: Number(PORT) }, resolve));
+console.log(`🚀 Server ready at http://localhost:${PORT}/`);
