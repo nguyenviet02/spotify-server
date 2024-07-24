c9077ee2aacb23ef9c94e86902cff0a9d3909339
Nguyen Viet <nguyenviet02.dev@gmail.com>
2024-07-24 19:07:17 +0700

    chore: change name variable

--- dist/db/dbConnector.js
+++ dist/db/dbConnector.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_STRING);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to DB');
     }
     catch (error) {
--- src/db/dbConnector.ts
+++ src/db/dbConnector.ts
@@ -6,7 +6,7 @@ import 'dotenv/config';
  **/
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_STRING);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to DB');
   } catch (error) {
     console.error('Error while connecting to DB');
